6ca3eb81a8a7133b3bf82529a943d48755a39dc0
DALERdeveloperWEP <cassidymlbbbbb@gmail.com>
2026-03-28 20:54:01 +0500

    admin panel

--- apps/registrations/admin.py
+++ apps/registrations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Registration
+
+
+@admin.register(Registration)
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'event', 'status', 'registered_at', 'cancelled_at')
+    search_fields = ('user__username', 'event__title')
+    ordering = ('id',)
--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    ordering = ('id',)
+    
+    
